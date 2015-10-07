b428bca79aa432580b1f4047211499a1de641928
David Tran <eternallite@users.noreply.github.com>
2015-10-07 09:43:17 -0400

    Add force option to _setPreferredCellCount to force updating cell count

--- index.js
+++ index.js
@@ -291,8 +291,8 @@ function GridView(params) {
 		_this.gridSolution = _gridSolution;
 	}
 
-	function _setPreferredCellCount(val) {
-		if(_lastPreferredCellCount === val) return;
+	function _setPreferredCellCount(val, force) {
+		if(_lastPreferredCellCount === val && !force) return;
 		_lastPreferredCellCount = val;
 		_gridLayout.setPreferredCellCount(val);
 		_solveGrid();
